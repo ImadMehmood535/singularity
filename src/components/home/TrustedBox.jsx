631476be31483b2078e0985ba4e30cec472aa6b7
Imad-Ocreateo <imad@ocreateo.com>
2024-06-21 17:30:34 +0500

    testing

--- src/components/home/TrustedBox.jsx
+++ src/components/home/TrustedBox.jsx
@@ -4,21 +4,23 @@ import React from "react";
 const TrustedBox = ({ item }) => {
   return (
     <div className="p-[1px] customLink2 relative">
- <div className="w-full  z-20 relative  px-7 py-5 flex flex-col gap-3 trustedGradient ">
-      <div className="h-[80px] relative w-full">
-        <Image
-          src={item?.pic}
-          alt={item?.head}
-          width={60}
-          height={40}
-          className="absolute bottom-0"
-        />
+      <div className="w-full  z-20 relative  px-7 py-5 flex flex-col gap-3 trustedGradient ">
+        <div className="h-[80px] relative w-full">
+          <Image
+            src={item?.pic}
+            alt={item?.head}
+            width={60}
+            height={40}
+            quality={75}
+            loading="eager"
+            fetchPriority="high"
+            className="absolute bottom-0"
+          />
+        </div>
+        <h3 className="text-themeTextGray-0">{item?.head}</h3>
+        <p className="text-sm text-themeTextGray-0 h-[70px]">{item?.para}</p>
       </div>
-      <h3 className="text-themeTextGray-0">{item?.head}</h3>
-      <p className="text-sm text-themeTextGray-0 h-[70px]">{item?.para}</p>
     </div>
-    </div>
-   
   );
 };
 
--- src/components/home/scrollBoxs/SBox.jsx
+++ src/components/home/scrollBoxs/SBox.jsx
@@ -3,8 +3,16 @@ import React from "react";
 
 const SBox = ({ pic, head, paragh }) => {
   return (
-    <div className="max-w-[300px] border-4 border-gray-400/70 font-light text-white w-full p-8 bg-gray-500/50 ra rounded-[25px] h-[320px]         ">
-      <Image src={pic} alt="pic" width={100} height={100} />
+    <div className="max-w-[300px] border-4 border-black/10 font-light text-white w-full p-8 bg-gray-500/50 ra rounded-[25px] h-[350px]         ">
+      <Image
+        src={pic}
+        alt="pic"
+        width={100}
+        height={100}
+        quality={75}
+        loading="eager"
+        fetchPriority="high"
+      />
       <h2 className="  mt-6 py-4 ">{head}</h2>
       <p className="text-[12px]">{paragh}</p>
     </div>

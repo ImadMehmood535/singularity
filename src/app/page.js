fb91664ada7d470cfa122b4400caf0abcede019e
Imad-Ocreateo <imad@ocreateo.com>
2024-06-21 17:32:47 +0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
-import HomeBanner from "@/components/home/HomeBanner";
-import ScrollHome from "@/components/home/ScrollHome";
-import TrustedPartners from "@/components/home/TrustedPartners";
+import dynamic from 'next/dynamic';
+import HomeBanner from '@/components/home/HomeBanner';
+
+// Dynamically import components
+const ScrollHome = dynamic(() => import('@/components/home/ScrollHome'), {
+  ssr: false, // Optional: If you want to disable SSR for this component
+  loading: () => <p>Loading...</p> // Optional: Loading component
+});
+
+const TrustedPartners = dynamic(() => import('@/components/home/TrustedPartners'), {
+  ssr: false, // Optional: If you want to disable SSR for this component
+  loading: () => <p>Loading...</p> // Optional: Loading component
+});
 
 export default function Home() {
   return (
     <>
       <HomeBanner />
-    
+      <TrustedPartners />
+      <ScrollHome />
     </>
   );
 }
